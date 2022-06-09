a89d8b39220a040f6fbcb21784efa298ca315e3f
Ali Zedan <Ali.Zedan@norgesgruppen.no>
2022-06-09 13:53:01 +0200

    Feat:Added Image service

---
+++ Eureka-Server/Image-service/src/main/java/Image.java
@@ -0,0 +1,36 @@
+class Image {
+
+    public Image(int id, String tittle, String url) {
+        this.id = id;
+        this.tittle = tittle;
+        this.url = url;
+    }
+    private int id;
+    private String tittle;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
---
+++ Eureka-Server/Image-service/src/test/java/com/example/imageservice/ImageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
